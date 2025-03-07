d2973e9040f5653a8d74711bc02605fa1b5c4c6a
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-03-07 23:37:02 +0700

    Implement Tokenizer interface and Tiktokenizer concrete class

---
+++ src/tokenizer.py
@@ -0,0 +1,40 @@
+'''Contains text encoder/decoder'''
+
+import abc
+
+import tiktoken
+
+
+class Tokenizer(metaclass=abc.ABCMeta):
+    '''Encodes/decodes string into tensor of token ids and vice versa'''
+
+    @classmethod
+    def __subclasshook__(cls, subclass: type, /) -> bool:
+        return (hasattr(subclass, 'encode') and
+                callable(subclass.encode) and
+                hasattr(subclass, 'decode') and
+                callable(subclass.decode))
+
+    @abc.abstractmethod
+    def encode(self, text: str, *args, **kwargs) -> list[int]:
+        '''Encodes text into a tensor of integers'''
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def decode(self, token_ids: list[int], *args, **kwargs) -> str:
+        '''Decodes an encoded text back to string'''
+        raise NotImplementedError
+
+class TiktokenTokenizer(Tokenizer):
+    '''Uses tiktoken from GPT'''
+
+    def __init__(self, encoding_name: str) -> None:
+        super().__init__()
+
+        self.__tokenizer = tiktoken.get_encoding(encoding_name)
+
+    def encode(self, text: str, *args, **kwargs) -> list[int]:
+        return self.__tokenizer.encode(text, *args, **kwargs)
+
+    def decode(self, token_ids: list[int], *args, **kwargs) -> str:
+        return self.__tokenizer.decode(token_ids, *args, **kwargs)
