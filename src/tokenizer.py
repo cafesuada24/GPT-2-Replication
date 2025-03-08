01dee4a63cd30265873261e118c24a043b1b6f88
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-03-08 23:19:18 +0700

    Implement encoder and decoder for text to tensor and vice versa

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -2,6 +2,7 @@
 
 import abc
 
+import torch
 import tiktoken
 
 
@@ -51,3 +52,35 @@ def get_tokenizer(tokenizer_name: str, init_args: dict):
             return TiktokenTokenizer(**init_args)
         case _:
             raise ValueError(f"Unsupported tokenizer: {tokenizer_name}")
+
+def text_to_token_ids(
+        text: str,
+        tokenizer: Tokenizer, *,
+        allowed_special: set | None = None) -> torch.Tensor:
+    """
+    Converts text into a tensor of token ids
+
+    Inputs:
+        text (str): raw text to convert
+        tokenizer (Tokenizer): converter
+        allowed_special (set): special characters to be included in dictionary. Default is None
+    Outputs:
+        Tensor: Shape (batch(1), num tokens)
+    """
+
+    encoded = tokenizer.encode(text, allowed_special=allowed_special)
+    return torch.tensor(encoded).unsqueeze(0)
+
+def token_ids_to_text(token_ids: torch.Tensor, tokenizer: Tokenizer) -> str:
+    """
+    Converts token ids back to raw text
+
+    Inputs:
+        token_ids (Tensor): token ids of shape (batch size (1), num tokens)
+        tokenizer (Tokenizer): converter
+    Outputs:
+        str: raw text
+    """
+
+    token_ids = token_ids.squeeze(0)
+    return tokenizer.decode(token_ids.tolist())
