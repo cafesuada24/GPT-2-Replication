dd3e0e5c4fbdad8a8b22f83ce56e98081a00a4cf
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-03-09 22:02:43 +0700

    Implement Transformer and its test cases

---
+++ src/model/transformer.py
@@ -0,0 +1,45 @@
+"""This module contains implementation of a Transformer block"""
+
+from omegaconf import DictConfig
+import torch
+from torch import nn
+
+from .attention import MultiHeadAttention, MultiHeadAttentionConfig
+from .layernorm import LayerNorm
+from .feedforward import FeedForward
+
+class Transformer(nn.Module):
+    """"""
+
+    def __init__(self, config: DictConfig) -> None:
+        super().__init__()
+        mha_config = MultiHeadAttentionConfig(
+            d_in=config.d_emb,
+            d_out=config.d_emb,
+            n_heads=config.n_heads,
+            context_length=config.context_length,
+            dropout=config.drop_rate,
+            qkv_bias=config.qkv_bias,
+        )
+        self.layernorm1 = LayerNorm(config.d_emb)
+        self.layernorm2 = LayerNorm(config.d_emb)
+        self.mha = MultiHeadAttention(mha_config)
+        self.feedforward = FeedForward(config.d_emb)
+        self.dropout = nn.Dropout(config.drop_rate)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """"""
+ 
+        shortcut = x
+        x = self.layernorm1(x)
+        x = self.mha(x)
+        x = self.dropout(x)
+        x = x + shortcut
+
+        shortcut = x
+        x = self.layernorm2(x)
+        x = self.feedforward(x)
+        x = self.dropout(x)
+        x = x + shortcut
+
+        return x
---
+++ test/test_transformer.py
@@ -0,0 +1,47 @@
+import pytest
+from omegaconf import OmegaConf
+import torch
+
+from src.model.transformer import Transformer
+
+@pytest.fixture
+def config():
+    return OmegaConf.create({
+        'vocab_size': 50257, 
+        'context_length': 1024,
+        'drop_rate': 0.1,
+        'qkv_bias': False,
+        'd_emb': 768,
+        'n_layers': 12,
+        'n_heads': 12,
+    })
+
+@pytest.fixture
+def n_batches():
+    return 2
+
+@pytest.fixture
+def n_tokens():
+    return 5
+
+def test_transformer_forward(config, n_batches, n_tokens):
+    trf = Transformer(config)
+
+    x = torch.randn(n_batches, n_tokens, config.d_emb)
+
+    output = trf(x)
+
+    assert output.shape == (n_batches, n_tokens, config.d_emb)
+
+# def test_transformer_backprop(config, n_batches, n_tokens):
+#     """Ensure TransformerBlock supports backpropagation."""
+#
+#     trf = Transformer(config)
+#
+#     x = torch.randn(n_batches, n_tokens, config.d_emb)
+#
+#     output = trf(x)
+#     loss = output.sum()
+#     loss.backward()
+#
+#     assert x.grad is not None, "Gradients are not flowing properly."
