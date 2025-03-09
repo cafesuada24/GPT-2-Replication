2536b0e45de66245b2028e3c25d4176fa8994f19
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-03-09 17:20:48 +0700

    Implement LayerNorm

---
+++ src/model/layernorm.py
@@ -0,0 +1,24 @@
+"""This module implements Normalization Layer"""
+
+import torch
+from torch import nn
+
+
+class LayerNorm(nn.Module):
+    """"""
+
+    def __init__(self, emb_dim: int) -> None:
+        super().__init__()
+
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """"""
+
+        x_mean = x.mean(dim=-1, keepdim=True)
+        x_var = x.var(dim=-1, keepdim=True, unbiased=False)
+        x_norm = (x - x_mean) / torch.sqrt(x_var + self.eps)
+
+        return self.scale * x + self.shift
