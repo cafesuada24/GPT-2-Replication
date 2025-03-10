a874ac4b77d2cf75734ab95955f4dc0f60049d0a
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-03-10 10:08:15 +0700

    Implement GPTDataset class

---
+++ src/dataset.py
@@ -0,0 +1,35 @@
+""""""
+
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+
+from .tokenizer import Tokenizer
+
+
+class GPTDataset(Dataset):
+    def __init__(
+        self,
+        raw_text: str,
+        tokenizer: Tokenizer,
+        max_sequence_len: int,
+        stride: int = 1,
+    ) -> None:
+        super().__init__()
+
+        tokens = tokenizer.encode(raw_text, allowed_special={"<|endoftext|>"})
+
+        self.inputs = []
+        self.targets = []
+
+        for index in range(len(tokens) - max_sequence_len, stride):
+            self.inputs.append(torch.tensor(tokens[index : index + max_sequence_len]))
+            self.targets.append(
+                torch.tensor(tokens[index + 1 : index + max_sequence_len + 1])
+            )
+
+    def __len__(self):
+        return len(self.inputs)
+
+    def __getitem__(self, index) -> tuple[torch.Tensor, torch.Tensor]:
+        return self.inputs[index], self.targets[index]
