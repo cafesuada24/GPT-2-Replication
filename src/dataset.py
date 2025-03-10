43d72f88f1e7bd33a79525520fbb8431e26b0602
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-03-10 10:34:08 +0700

    Move dataset and dataloader arguments to a separated object

--- src/dataset.py
+++ src/dataset.py
@@ -1,18 +1,34 @@
 """"""
 
+from dataclasses import dataclass
+
 import torch
 from torch.utils.data import Dataset, DataLoader
 
 from .tokenizer import Tokenizer
 
 
+@dataclass
+class DataConfig:
+    """"""
+
+    raw_text: str
+    max_sequence_len: int = 256
+    batch_size: int = 4
+    stride: int = 128
+    shuffle: bool = True
+    drop_last: bool = True
+    num_workers: int = 0
+
+
 class GPTDataset(Dataset):
+    """"""
+
     def __init__(
         self,
         raw_text: str,
         tokenizer: Tokenizer,
-        max_sequence_len: int,
-        stride: int = 1,
+        data_config: DataConfig,
     ) -> None:
         super().__init__()
 
@@ -21,10 +37,16 @@ class GPTDataset(Dataset):
         self.inputs = []
         self.targets = []
 
-        for index in range(len(tokens) - max_sequence_len, stride):
-            self.inputs.append(torch.tensor(tokens[index : index + max_sequence_len]))
+        for index in range(
+            len(tokens) - data_config.max_sequence_len, data_config.stride
+        ):
+            self.inputs.append(
+                torch.tensor(tokens[index : index + data_config.max_sequence_len])
+            )
             self.targets.append(
-                torch.tensor(tokens[index + 1 : index + max_sequence_len + 1])
+                torch.tensor(
+                    tokens[index + 1 : index + data_config.max_sequence_len + 1]
+                )
             )
 
     def __len__(self):
@@ -35,20 +57,21 @@ class GPTDataset(Dataset):
 
 
 def create_dataloader(
-    raw_text: str,
-    tokenizer: Tokenizer,
-    max_sequence_len: int,
-    batch_size: int,
-    stride: int,
-    shuffle: bool,
+    raw_text: str, tokenizer: Tokenizer, data_config: DataConfig
 ) -> DataLoader:
+    """"""
 
     dataset = GPTDataset(
         raw_text=raw_text,
         tokenizer=tokenizer,
-        max_sequence_len=max_sequence_len,
-        stride=stride,
+        data_config=data_config,
     )
-    dataloader = DataLoader(dataset, shuffle=shuffle, batch_size=batch_size)
+    dataloader = DataLoader(
+        dataset,
+        shuffle=data_config.shuffle,
+        batch_size=data_config.batch_size,
+        drop_last=data_config.drop_last,
+        num_workers=data_config.num_workers,
+    )
 
     return dataloader
