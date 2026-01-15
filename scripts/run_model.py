a299c3f2e9aa3ed263f1dc11a138da929e02280b
Cafesuada <108068915+cafesuada24@users.noreply.github.com>
2026-01-15 14:15:25 +0700

    support running OpenAI GPT2 trained weights

--- scripts/run_model.py
+++ scripts/run_model.py
@@ -3,11 +3,13 @@
 """Run llm."""
 
 import argparse
+from typing import Any
 
 import torch
 
+from scripts.gpt_download import download_and_load_gpt2
 from src.inference import generate
-from src.model.gpt2 import GPT2
+from src.model.gpt2 import GPT2, load_weights_into_gpt
 from src.tokenizer import TiktokenTokenizer, text_to_token_ids, token_ids_to_text
 from src.utils.config import config
 from src.utils.logger import get_logger
@@ -15,24 +17,48 @@ from src.utils.logger import get_logger
 _logger = get_logger('Model')
 
 
-def _load_model(file: str, device: str) -> GPT2:
+def _load_model(args: argparse.Namespace, device: str) -> tuple[dict[str, Any], GPT2]:
     if device == 'cuda' and not torch.cuda.is_available():
         raise ValueError('Cuda is not available.')
     _logger.info(f'Use device: {device}')
 
-    checkpoint = torch.load(file, map_location=device)
+    settings = None
+    params = None
+    checkpoint = None
+
+    if args.custom_model:
+        checkpoint = torch.load(args.model_path, map_location=device)
+        settings = checkpoint['config']
+        if 'context_length' not in settings:
+            settings['context_length'] = config.model.config.context_length
+        config.model.config.context_length = settings['context_length']
+    else:
+        settings, params = download_and_load_gpt2(
+            model_size=args.gpt_model,
+            models_dir=args.model_path,
+        )
+        config.model.config.context_length = settings['n_ctx']  # GPT use context length of 1024
+        config.model.config.qkv_bias = True
+        settings['context_length'] = settings['n_ctx']
+
     model = GPT2(config.model.config)
-    model.load_state_dict(checkpoint)
+
+    if args.custom_model:
+        model.load_state_dict(checkpoint['model_state_dict'])
+    else:
+        load_weights_into_gpt(model, params)
+
     model.to(device)
     model.eval()
 
-    return model
+    return settings, model
 
 
 def main(args: argparse.Namespace) -> None:
     device = 'cuda' if args.cuda else 'cpu'
-    model = _load_model(args.model, device)
+    settings, model = _load_model(args, device)
     tokenizer = TiktokenTokenizer()
+
     print('Model > Hi, how can I help you')
 
     try:
@@ -42,7 +68,7 @@ def main(args: argparse.Namespace) -> None:
                 model=model,
                 token_ids=text_to_token_ids(prompt, tokenizer).to(device),
                 max_new_tokens=args.max_new_tokens,
-                context_size=args.context_length,
+                context_size=settings['context_length'],
                 temperature=args.temperature,
             )
             print('GPT >', token_ids_to_text(response, tokenizer))
@@ -53,23 +79,31 @@ def main(args: argparse.Namespace) -> None:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='GPT2 Launch')
 
+    model_mode_group = parser.add_mutually_exclusive_group(required=True)
+
+    model_mode_group.add_argument(
+        '--gpt_model',
+        help='Use OpenAI GPT2 weight',
+        default='124M',
+        choices=['124M', '355M', '774M', '1558M'],
+    )
+
+    model_mode_group.add_argument(
+        '--custom_model',
+        action='store_true',
+        help='Use custom model',
+    )
+
     parser.add_argument(
-        '-m',
-        '--model',
-        help='Model pkl file path',
-        default=config.paths.model_dir + 'gpt2.pkl',
+        '--model_path',
+        help='Model path',
+        required=True,
     )
 
     parser.add_argument(
         '--max_new_tokens',
         help='Max new tokens to generate',
         default=200,
-    )
-
-    parser.add_argument(
-        '--context_length',
-        help='Context size',
-        default=config.model.config.context_length,
     )
 
     parser.add_argument(
--- src/model/gpt2.py
+++ src/model/gpt2.py
@@ -1,11 +1,110 @@
-"""This module contains implementation of GPT2 model"""
+"""This module contains implementation of GPT2 model."""
 
-from omegaconf import DictConfig
+from typing import Any
+
+import numpy as np
 import torch
+from omegaconf import DictConfig
 from torch import nn
 
 from .layernorm import LayerNorm
 from .transformer import Transformer
+
+
+def _assign(left: torch.Tensor, right: np.ndarray) -> torch.nn.Parameter:
+    if left.shape != right.shape:
+        raise ValueError(
+            f'Shape mismatch. Left: {left.shape}, ',
+            f'Right: {right.shape}',
+        )
+    return torch.nn.Parameter(torch.tensor(right))
+
+
+def load_weights_into_gpt(gpt: 'GPT2', params: dict[str, Any]) -> None:
+    gpt.pos_embedding.weight = _assign(gpt.pos_embedding.weight, params['wpe'])
+    gpt.token_embedding.weight = _assign(gpt.token_embedding.weight, params['wte'])
+
+    for b in range(len(params['blocks'])):
+        q_w, k_w, v_w = np.split(
+            (params['blocks'][b]['attn']['c_attn'])['w'],
+            3,
+            axis=-1,
+        )
+        gpt.trf_blocks[b].mha.w_query.weight = _assign(
+            gpt.trf_blocks[b].mha.w_query.weight,
+            q_w.T,
+        )
+        gpt.trf_blocks[b].mha.w_key.weight = _assign(
+            gpt.trf_blocks[b].mha.w_key.weight,
+            k_w.T,
+        )
+        gpt.trf_blocks[b].mha.w_value.weight = _assign(
+            gpt.trf_blocks[b].mha.w_value.weight,
+            v_w.T,
+        )
+        q_b, k_b, v_b = np.split(
+            (params['blocks'][b]['attn']['c_attn'])['b'],
+            3,
+            axis=-1,
+        )
+        gpt.trf_blocks[b].mha.w_query.bias = _assign(
+            gpt.trf_blocks[b].mha.w_query.bias,
+            q_b,
+        )
+        gpt.trf_blocks[b].mha.w_key.bias = _assign(
+            gpt.trf_blocks[b].mha.w_key.bias,
+            k_b,
+        )
+        gpt.trf_blocks[b].mha.w_value.bias = _assign(
+            gpt.trf_blocks[b].mha.w_value.bias,
+            v_b,
+        )
+        gpt.trf_blocks[b].mha.out_proj.weight = _assign(
+            gpt.trf_blocks[b].mha.out_proj.weight,
+            params['blocks'][b]['attn']['c_proj']['w'].T,
+        )
+        gpt.trf_blocks[b].mha.out_proj.bias = _assign(
+            gpt.trf_blocks[b].mha.out_proj.bias,
+            params['blocks'][b]['attn']['c_proj']['b'],
+        )
+
+        gpt.trf_blocks[b].feedforward.layers[0].weight = _assign(
+            gpt.trf_blocks[b].feedforward.layers[0].weight,
+            params['blocks'][b]['mlp']['c_fc']['w'].T,
+        )
+        gpt.trf_blocks[b].feedforward.layers[0].bias = _assign(
+            gpt.trf_blocks[b].feedforward.layers[0].bias,
+            params['blocks'][b]['mlp']['c_fc']['b'],
+        )
+        gpt.trf_blocks[b].feedforward.layers[2].weight = _assign(
+            gpt.trf_blocks[b].feedforward.layers[2].weight,
+            params['blocks'][b]['mlp']['c_proj']['w'].T,
+        )
+        gpt.trf_blocks[b].feedforward.layers[2].bias = _assign(
+            gpt.trf_blocks[b].feedforward.layers[2].bias,
+            params['blocks'][b]['mlp']['c_proj']['b'],
+        )
+
+        gpt.trf_blocks[b].layernorm1.scale = _assign(
+            gpt.trf_blocks[b].layernorm1.scale,
+            params['blocks'][b]['ln_1']['g'],
+        )
+        gpt.trf_blocks[b].layernorm1.shift = _assign(
+            gpt.trf_blocks[b].layernorm1.shift,
+            params['blocks'][b]['ln_1']['b'],
+        )
+        gpt.trf_blocks[b].layernorm2.scale = _assign(
+            gpt.trf_blocks[b].layernorm2.scale,
+            params['blocks'][b]['ln_2']['g'],
+        )
+        gpt.trf_blocks[b].layernorm2.shift = _assign(
+            gpt.trf_blocks[b].layernorm2.shift,
+            params['blocks'][b]['ln_2']['b'],
+        )
+
+        gpt.final_norm.scale = _assign(gpt.final_norm.scale, params['g'])
+        gpt.final_norm.shift = _assign(gpt.final_norm.shift, params['b'])
+        gpt.linear_output.weight = _assign(gpt.linear_output.weight, params['wte'])
 
 
 class GPT2(nn.Module):
@@ -20,7 +119,7 @@ class GPT2(nn.Module):
         self.pos_embedding = nn.Embedding(config.context_length, config.d_emb)
         self.dropout = nn.Dropout(config.drop_rate)
         self.trf_blocks = nn.Sequential(
-            *(Transformer(config) for _ in range(config.n_layers))
+            *(Transformer(config) for _ in range(config.n_layers)),
         )
         self.final_norm = LayerNorm(config.d_emb)
         self.linear_output = nn.Linear(config.d_emb, config.vocab_size, bias=False)
@@ -35,13 +134,11 @@ class GPT2(nn.Module):
             torch.arange(
                 n_tokens,
                 device=x.device,
-            )
+            ),
         )
 
         x = token_emb + pos_emb
         x = self.dropout(x)
         x = self.trf_blocks(x)
         x = self.final_norm(x)
-        logits = self.linear_output(x)
-
-        return logits
+        return self.linear_output(x)
